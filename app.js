088ab330d52b27c54f275e635a8a7a5d4b73f9b1
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-15 11:29:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+//Task # 38
+// Cities: write a function called describe_city() that accept the name of a city and its country. the function should print a
+//Simple sentence, such as Karachi is in Pakistan.Give the parameter for the country a default value. call your function for
+// Three different cities atleast one of which is not in the default country.
+function describe_city(city, country = 'Pakistan') {
+    console.log(` ${city} is in ${country}`);
+}
+describe_city('Karachi'); // Default sentence
+describe_city('France', 'Europe');
+describe_city('Lahore', 'Pakistan');
+export {};
